c6832637fae8656046c521d5482fbb96cbb6f83c
Francisco Alvarez <fjab76@gmail.com>
2019-06-11 00:09:53 +0100

    Refactoring/fixing MissingNumberOnArray

--- scala/src/main/scala/challenges/missingnumberarray/MissingNumberOnArray.scala
+++ scala/src/main/scala/challenges/missingnumberarray/MissingNumberOnArray.scala
@@ -1,120 +1,127 @@
 package challenges.missingnumberarray
 
 import java.util
+
 import scala.annotation.tailrec
 import scala.collection.GenSeq
 import scala.collection.parallel.mutable.ParArray
 
 
+/**
+  * Given an array 'arr' containing k distinct numbers taken from 1,2 ... n with k < n, find the n - k missing numbers
+  */
 object MissingNumberOnArray {
 
 
   /**
-    * Given an array 'arr' of all except one integers between 1 and 'max', find the missing one
+    * Solution only valid when n - k == 1
+    *
+    * Implementation details:
+    * 1. calculate the sum of 1 ... n using the formula of the sum of the first n natural numbers
+    * 2. calculate the sum of all the elements in the array
+    * 3. the difference of both sums is the solution
     */
-  def solution1(arr: GenSeq[Long], max: Long): Long = max*(max+1)/2 - arr.sum
+  def solution1(arr: GenSeq[Long], n: Long): Long = n*(n+1)/2 - arr.sum
 
   /**
-    * Given an array 'arr' of all except n integers between 1 and 'max', find the n missing ones
+    * Generic solution
+    *
+    * Implementation details:
+    * 1. sort the elements of the array
+    * 2. traverse the array and for every element a[n] check if a[n]+1 == a[n+1]: if not, the sequence of
+    * numbers between a[n] and a[n+1] is the solution
     */
-  def solutionN(arr: Array[Int], max: Int): List[Int] = {
-    val arrSorted = arr.sorted
+  def solutionN(arr: Array[Int], n: Int): List[Int] = {
+    util.Arrays.parallelSort(arr) //Using Java alternative as Scala's ParArray does not have sorting methods
+    val arrSorted = arr
 
     @tailrec
-    def traverseArray(idx: Int, misses: List[Int]): List[Int] = {
+    def traverseArray(idx: Int, missingValues: List[Int]): List[Int] = {
       if(arrSorted.length-1 == idx)
-        misses
+        missingValues
       else
-        traverseArray(idx+1, if(arrSorted(idx)+1 != arrSorted(idx+1)) (arrSorted(idx) + 1 until arrSorted(idx + 1)).toList ++ misses else misses)
+        traverseArray(idx+1, if(arrSorted(idx)+1 != arrSorted(idx+1))  ((arrSorted(idx + 1) - 1) to(arrSorted(idx) + 1,-1)).toList ::: missingValues else missingValues)
 
     }
 
-    val isFirstValueMissing = if(arrSorted(0) != 1) (arrSorted(0)-1 to(1,-1)).toList else List()
-    val missingValues = traverseArray(0, isFirstValueMissing)
-    val isLastValueMissing = if(arrSorted(arrSorted.length-1) != max)  (max to(arrSorted(arrSorted.length-1)+1,-1)).toList else List()
-    (isLastValueMissing ++ missingValues).reverse
+    val firstMissingValues = if(arrSorted(0) != 1) (arrSorted(0)-1 to(1,-1)).toList else List()
+    val missingValues = traverseArray(0, firstMissingValues)
+    val lastMissingValues = if(arrSorted(arrSorted.length-1) != n)  (n to(arrSorted(arrSorted.length-1)+1,-1)).toList else List()
+    (lastMissingValues ::: missingValues).reverse
   }
 
   /**
-    * Given an array 'arr' of all except n integers between 1 and 'max', find the n missing ones
+    * Generic solution
+    *
+    * Implementation details:
+    * 1. create an array 'b' of n boolean elements with all elements initialised to false (such an array is a BitSet)
+    * 2. map each element of the original array 'arr' to its corresponding position in the new array 'b': the value of
+    * an element in 'arr' minus 1 represents the index of its position in 'b' (as 'arr' is a 1-indexed array and 'b'
+    * a 0-indexed one)
+    * 3. the elements in 'b' that can be mapped to an element in 'arr' are set to true
+    * 4. the index plus 1 of the remaining false values in 'b' are the solution
     */
-  def solutionNOptimised(arr: GenSeq[Int], max: Int): List[Int] = {
+  def solutionNOptimised(arr: GenSeq[Int], n: Int): List[Int] = {
 
     @tailrec
-    def missingValues(max: Int, lastMissingIndex: Int, misses: List[Int], bitSet: util.BitSet): List[Int] = {
+    def missingValues(lastMissingIndex: Int, misses: List[Int], bitSet: util.BitSet): List[Int] = {
       val missingIndex = bitSet.nextClearBit(lastMissingIndex)
-      if(missingIndex == max)
+      if(missingIndex == n)
         misses
       else
-        missingValues(max, missingIndex+1, (missingIndex+1) :: misses, bitSet)
+        missingValues(missingIndex+1, (missingIndex+1) :: misses, bitSet)
     }
 
-    val bitSet = new java.util.BitSet(max)
+    val bitSet = new java.util.BitSet(n)
     arr.foreach(i => bitSet.set(i - 1))
-    missingValues(max, 0, Nil, bitSet).reverse
+    missingValues(0, Nil, bitSet).reverse
   }
 
 }
 
+/**
+  * Benchmark, using Scalameter, to compare the performance of the 3 implementations
+  */
 object MissingNumberOnArrayBenchmark {
 
   import org.scalameter.{Key, Warmer, config}
   import MissingNumberOnArray._
 
-  val max: Int = 10000000
-  val arr: Array[Int] = ((1 to max/2) ++ (max/2+2 to max)).toArray
+  val n: Int = 10000000
+  //By construction, the missing number is max/2+1
+  val arr: Array[Int] = ((1 to n/2) ++ (n/2+2 to n)).toArray
   val arrPar: ParArray[Int] = arr.par
   val arrLong = arr.map(_.toLong)
   val arrLongPar = arrLong.par
 
   val standardConfig = config(
-    Key.exec.minWarmupRuns -> 10,
-    Key.exec.maxWarmupRuns -> 20,
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 30,
     Key.exec.benchRuns -> 50,
-    Key.verbose -> true
+    Key.verbose -> false
   ) withWarmer(new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
 
-    println(solution1(arrLong, max))
-    println(solution1(arrLongPar, max))
-    println(solutionN(arr, max))
-    println(solutionNOptimised(arr, max))
-    println(solutionNOptimised(arrPar, max))
+    println(solution1(arrLong, n))
+    println(solutionN(arr, n))
+    println(solutionNOptimised(arrPar, n))
 
-    val parallelTime = standardConfig.measure{
-      arr.par
-    }
-
-    val reverseTime = standardConfig.measure{
-      arr.reverse
-    }
-
-    val solution1Time = standardConfig.measure{
-      solution1(arrLong, max)
-    }
     val solution1ParTime = standardConfig.measure{
-      solution1(arrLongPar, max)
+      solution1(arrLongPar, n)
     }
 
     val solutionNTime = standardConfig.measure{
-      solutionN(arr, max)
+      solutionN(arr, n)
     }
 
-    val solutionNOptimisedTime = standardConfig.measure{
-      solutionNOptimised(arr, max)
-    }
     val solutionNOptimisedParTime = standardConfig.measure{
-      solutionNOptimised(arrPar, max)
+      solutionNOptimised(arrPar, n)
     }
 
-    println(s"parallelTime: $parallelTime")
-    println(s"reverseTime: $reverseTime")
-    println(s"solution1Time: $solution1Time")
-    println(s"solution1ParTime: $solution1ParTime")
-    println(s"solutionNTime: $solutionNTime")
-    println(s"solutionNOptimisedTime: $solutionNOptimisedTime")
-    println(s"solutionNOptimisedParTime: $solutionNOptimisedParTime")
+    println(s"solution1ParTime: $solution1ParTime") //40ms
+    println(s"solutionNTime: $solutionNTime") //30ms
+    println(s"solutionNOptimisedParTime: $solutionNOptimisedParTime") //14ms
   }
 }
 
--- scala/src/test/scala/challenges/missingnumberarray/MissingNumberOnArrayTest.scala
+++ scala/src/test/scala/challenges/missingnumberarray/MissingNumberOnArrayTest.scala
@@ -8,7 +8,7 @@ import org.scalatest.prop.PropertyChecks
 class MissingNumberOnArrayTest extends FunSpec with PropertyChecks{
 
   /*
-    Shrinking ignores restrictions of your generator, e.g. shrinking a Gen.choose(1, 10) might try to falsify the
+    Shrinking ignores restrictions of the generator, e.g. shrinking a Gen.choose(1, 10) might try to falsify the
     property with values such as 0, and -1
 
     With this statement, shrinking is disabled
@@ -21,20 +21,20 @@ class MissingNumberOnArrayTest extends FunSpec with PropertyChecks{
     * 0 < l <= maxLength
     *
     * For instance, given maxLength=6:
-    * + l=5 -> [1,2,3,4,5], missing value=[]
-    * + l=5 -> [1,2,3,4,6], missing value=[6]
+    * + l=5 -> [1,2,3,4,5], missing value=[6]
+    * + l=5 -> [1,2,3,4,6], missing value=[5]
     * + l=6 -> [1,2,3,4,5,6], missing value=[]
     *
     */
-  def missingNumbersOnArrayGen(maxLength: Int): Gen[Array[Int]] = for {
-    l <- Gen.choose(1, maxLength)
-    is <- Gen.pick(l, 1 to maxLength)
+  def missingNumbersOnArrayGen(n: Int): Gen[Array[Int]] = for {
+    k <- Gen.choose(1, n)
+    is <- Gen.pick(k, 1 to n)
   } yield is.toArray
 
-  val maxLength = 15
-  forAll (missingNumbersOnArrayGen(maxLength)) { arr =>
-    assert((arr ++ solutionN(arr, maxLength)).toList.sorted == (1 to maxLength).toList)
-    assert((arr ++ solutionNOptimised(arr, maxLength)).toList.sorted == (1 to maxLength).toList)
+  val n = 15
+  forAll (missingNumbersOnArrayGen(n)) { arr =>
+    assert((arr ++ solutionN(arr, n)).toList.sorted == (1 to n).toList)
+    assert(solutionNOptimised(arr, n) == solutionN(arr, n))
   }
 
   describe("one missing element") {
@@ -49,31 +49,31 @@ class MissingNumberOnArrayTest extends FunSpec with PropertyChecks{
     describe("given the array of integers [1,3,2,5,7,8] with elements between 1 and 8") {
 
       val arr = Array(1, 3, 2, 5, 7, 8)
-      val max = 8
+      val n = 8
       val expectedSolution = List(4, 6)
       it("the missing elements are 4 and 6") {
-        assert(solutionN(arr, max) == expectedSolution)
-        assert(solutionNOptimised(arr, max) == expectedSolution)
+        assert(solutionN(arr, n) == expectedSolution)
+        assert(solutionNOptimised(arr, n) == expectedSolution)
       }
     }
 
     describe("given the array of integers [4,3,2,5,7,6] with elements between 1 and 8") {
       val arr = Array(4,3,2,5,7,6)
-      val max = 8
+      val n = 8
       val expectedSolution = List(1,8)
       it("the missing elements are the extremes 1 and 8") {
-        assert(solutionN(arr, max) == expectedSolution)
-        assert(solutionNOptimised(arr, max) == expectedSolution)
+        assert(solutionN(arr, n) == expectedSolution)
+        assert(solutionNOptimised(arr, n) == expectedSolution)
       }
     }
 
     describe("given the array of integers [1,2,3] with elements between 1 and 10") {
       val arr = Array(1,2,3)
-      val max = 10
+      val n = 10
       val expectedSolution = List(4,5,6,7,8,9,10)
       it("the missing elements are 4,5,6,7,8,9,10)") {
-        assert(solutionN(arr, max) == expectedSolution)
-        assert(solutionNOptimised(arr, max) == expectedSolution)
+        assert(solutionN(arr, n) == expectedSolution)
+        assert(solutionNOptimised(arr, n) == expectedSolution)
       }
     }
   }
