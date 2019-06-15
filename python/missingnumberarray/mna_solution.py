156ce146b207421be501c0237e2042480f6d09d1
Francisco Alvarez <fjab76@gmail.com>
2019-06-15 11:47:37 +0100

    adding benchmark details

--- python/missingnumberarray/mna_solution.py
+++ python/missingnumberarray/mna_solution.py
@@ -48,8 +48,8 @@ if __name__ == '__main__':
     print(solution_n_optimised(test_data, n))
 
     t1 = timeit.repeat(lambda: solution1(test_data, n), repeat=3, number=1)
-    print(f't1: {t1}')
+    print(f't1: {t1} sec') # [0.05796466600440908, 0.058634756998799276, 0.05945016899931943]
     tn = timeit.repeat(lambda: solution_n(test_data, n), repeat=3, number=1)
-    print(f'tn: {tn}')
+    print(f'tn: {tn} sec')  # [1.6984177570047905, 1.6123321470004157, 1.627043018997938]
     tn_optimised = timeit.repeat(lambda: solution_n_optimised(test_data, n), repeat=3, number=1)
-    print(f'tn_optimised: {tn_optimised}')
+    print(f'tn_optimised: {tn_optimised} sec')  # [1.7067252099950565, 1.6756674749995, 1.654086376001942]
