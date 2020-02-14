523fac26415aa4e00fe4cf3b300da8092afb5a74
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2020-02-14 23:11:21 +0000

    negabinary refactoring

--- scala/src/main/scala/challenges/negabinary/NegaBinary.scala
+++ scala/src/main/scala/challenges/negabinary/NegaBinary.scala
@@ -11,63 +11,65 @@ object NegaBinary {
 
 
   /**
-    * Calculates base expansion of n in the base targetBase.
+    * Calculates base 'targetBase' expansion of a 10-based number.
     *
     * Notes:
-    * - n is 10-base number
+    * - n is an integer in base 10
     * - the leading digit of the base expansion is on the leftmost position
     * - the target base may be positive or negative and its absolute value cannot be greater than 10
     */
-  def from10(n: Int, targetBase: Int): String = {
+  def fromBase10ToTargetBase(n: Int, targetBase: Int): String = {
 
     assert(math.abs(targetBase) > 1 && math.abs(targetBase) <= 10,
       "1 results in an infinite calculation and >10 requires defining special symbols to represent values >10")
 
     @tailrec
-    def acc(n: Int, targetRepresentation: List[Int]): List[Int] = {
+    def acc(n: Int, expansionInTargetBase: List[Int]): List[Int] = {
+
+      if(targetBase > 0){
+        assert(n >= 0, "negative number cannot be expanded in a positive base")
+      }
+
       var q = n/targetBase
       var r = n%targetBase
 
-      if(targetBase < 0) {
-        if (r < 0) {
-          q += 1
-          r -= targetBase
-        }
+      //Note: operator % in Scala is based on truncated division, thus r < 0 only when the dividend is < 0
+      if (r < 0) {
+        q += 1
+        r -= targetBase
       }
 
-      if(q == 0) r :: targetRepresentation
-      else acc(q, r :: targetRepresentation)
+      if(q == 0) r :: expansionInTargetBase
+      else acc(q, r :: expansionInTargetBase)
     }
 
-    if(n < 0 && targetBase > 0){
-      "-" + acc(-n, Nil).mkString
-    }
-    else {
-      acc(n, Nil).mkString
-    }
+    def handleSign(n: Int, targetBase: Int) = if(n < 0 && targetBase > 0) ("-", -n) else ("", n)
+
+    val (sign, magnitude) = handleSign(n, targetBase)
+    sign + acc(magnitude, Nil).mkString
   }
 
   /**
-    * Calculates 10-base representation of n
+    * Calculates 10-base representation of a number defined in base originBase
     *
     * Notes:
     * - n is the base expansion in a given origin base
     * - the leading digit of the base expansion is on the leftmost position
     * - the origin base may be positive or negative (only bases between -10 and 10 can be handled)
     */
-  def to10(n: String, originBase: Int): Int = {
+  def fromOriginBaseToBase10(n: String, originBase: Int): Int = {
 
-    val isNegative = n.head == '-'
+    val sign = if(n.head == '-') "-" else ""
 
-    if(isNegative && originBase < 0){
+    if(sign == "-" && originBase < 0){
       throw new IllegalArgumentException("negative bases do not have -")
     }
 
-    def negateString(s: String) = if(isNegative) s.substring(1) else s
-    def negate(i: Int) = if(isNegative) -i else i
+    val nMagnitude = if(sign == "-") n.substring(1) else n
+    def applySign(i: Int) = if(sign == "-") -i else i
 
-    val valueWithPosition: Array[(Int, Int)] = negateString(n).reverse.toArray.map(Character.getNumericValue).zipWithIndex
-    negate(valueWithPosition.foldLeft(0) {
+    val valueWithPosition: Array[(Int, Int)] = nMagnitude.reverse.toArray.map(Character.getNumericValue).zipWithIndex
+    applySign(valueWithPosition.foldLeft(0) {
       case (acc, (value, position)) => acc + value * math.pow(originBase, position).toInt
     })
 
--- scala/src/test/scala/challenges/negabinary/NegaBinaryTest.scala
+++ scala/src/test/scala/challenges/negabinary/NegaBinaryTest.scala
@@ -8,110 +8,110 @@ import org.scalacheck.Gen
 class NegaBinaryTest extends FunSpec with PropertyChecks{
 
 
-  describe("from 10"){
+  describe("from base 10"){
 
-    describe("to 10"){
+    describe("to base 10"){
       it("-12 == -12"){
-        assert(from10(-12, 10) == "-12")
+        assert(fromBase10ToTargetBase(-12, 10) == "-12")
       }
     }
 
-    describe("to -10"){
+    describe("to base -10"){
 
       val targetBase = -10
       it("0 -> 0"){
-        assert(from10(0, targetBase) == "0")
+        assert(fromBase10ToTargetBase(0, targetBase) == "0")
       }
 
       it("1 -> 1"){
-        assert(from10(1, targetBase) == "1")
+        assert(fromBase10ToTargetBase(1, targetBase) == "1")
       }
 
       it("-15 -> 25"){
-        assert(from10(-15, targetBase) == "25")
+        assert(fromBase10ToTargetBase(-15, targetBase) == "25")
       }
 
       it("10 -> 190"){
-        assert(from10(10, targetBase) == "190")
+        assert(fromBase10ToTargetBase(10, targetBase) == "190")
       }
     }
 
-    describe("to -2"){
+    describe("to base -2"){
       val targetBase = -2
       it("0 -> 0"){
-        assert(from10(0, targetBase) == "0")
+        assert(fromBase10ToTargetBase(0, targetBase) == "0")
       }
 
       it("1 -> 1"){
-        assert(from10(1, targetBase) == "1")
+        assert(fromBase10ToTargetBase(1, targetBase) == "1")
       }
 
       it("-1 -> 11"){
-        assert(from10(-1, targetBase) == "11")
+        assert(fromBase10ToTargetBase(-1, targetBase) == "11")
       }
 
       it("-9 -> 1011"){
-        assert(from10(-9, targetBase) == "1011")
+        assert(fromBase10ToTargetBase(-9, targetBase) == "1011")
       }
 
       it("9 -> 11001"){
-        assert(from10(9, targetBase) == "11001")
+        assert(fromBase10ToTargetBase(9, targetBase) == "11001")
       }
     }
   }
 
 
-  describe("to 10"){
+  describe("to base 10"){
 
-    describe("from -10"){
+    describe("from base -10"){
       val originBase = -10
       it("0 -> 0"){
-        assert(to10("0", originBase) == 0)
+        assert(fromOriginBaseToBase10("0", originBase) == 0)
       }
 
       it("1 -> 1"){
-        assert(to10("1", originBase) == 1)
+        assert(fromOriginBaseToBase10("1", originBase) == 1)
       }
 
       it("25 -> -15"){
-        assert(to10("25", originBase) == -15)
+        assert(fromOriginBaseToBase10("25", originBase) == -15)
       }
 
       it("190 -> 10"){
-        assert(to10("190", originBase) == 10)
+        assert(fromOriginBaseToBase10("190", originBase) == 10)
       }
     }
 
-    describe("from -2"){
+    describe("from base -2"){
       val originBase = -2
       it("0 -> 0"){
-        assert(to10("0", originBase) == 0)
+        assert(fromOriginBaseToBase10("0", originBase) == 0)
       }
 
       it("1 -> 1"){
-        assert(to10("1", originBase) == 1)
+        assert(fromOriginBaseToBase10("1", originBase) == 1)
       }
 
       it("11 -> -1"){
-        assert(to10("11", originBase) == -1)
+        assert(fromOriginBaseToBase10("11", originBase) == -1)
       }
 
       it("1011 -> -9"){
-        assert(to10("1011", originBase) == -9)
+        assert(fromOriginBaseToBase10("1011", originBase) == -9)
       }
 
       it("11001 -> 9"){
-        assert(to10("11001", originBase) == 9)
+        assert(fromOriginBaseToBase10("11001", originBase) == 9)
       }
     }
 
-    describe("from 2"){
+    describe("from base 2"){
       it("110 -> 6"){
-        assert(to10("110", 2) == 6)
+        assert(fromOriginBaseToBase10("110", 2) == 6)
       }
 
       it("-110 -> -6"){
-        assert(to10("-110", 2) == -6)
+        assert(fromOriginBaseToBase10("-110", 2) == -6)
       }
     }
   }
@@ -139,12 +139,12 @@ class NegaBinaryTest extends FunSpec with PropertyChecks{
 
   forAll(positiveTargetBaseGenerator(100)) {
     case (n, targetBase) =>
-      assert(to10(from10(n, targetBase), targetBase) == n)
+      assert(fromOriginBaseToBase10(fromBase10ToTargetBase(n, targetBase), targetBase) == n)
   }
 
   forAll(negativeTargetBaseGenerator(100)) {
     case (n, targetBase) =>
-      assert(to10(from10(n, targetBase), targetBase) == n)
+      assert(fromOriginBaseToBase10(fromBase10ToTargetBase(n, targetBase), targetBase) == n)
   }
 
 
