5268536d99682daa9e528f9582da19736ee2e112
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-07-07 17:41:49 +0100

    tower hanoi docs

--- python/towerhanoi/hanoi_solution.py
+++ python/towerhanoi/hanoi_solution.py
@@ -1,5 +1,10 @@
 
 class Stack:
+    """
+    Stack implementation based on a list
+    The end of the list is the top of the stack
+    The elements of the stack must be comparable as the implementation does not allow to push an element on top of a smaller one
+    """
     def __init__(self, arr: list):
         self.arr = arr
 
@@ -15,18 +20,15 @@ class Stack:
         return len(self.arr)
 
 
-def solve(tower1: Stack, tower2: Stack, tower3: Stack) -> Stack:
+def solve(tower1: Stack, tower2: Stack, tower3: Stack):
     """
-    Solves the problem of the towers of Hanoi: moves elements from tower1 to tower3 using auxiliary tower2 to make
-    sure that, during the process, smaller disks are always on top of larger ones
-
-    Args:
-        tower1 (Stack): list of numbers representing the relative size of each element in descending order
-
-    Returns:
-        Stack: list of numbers representing the relative size of each element in ascending order
+    Solves the problem of the towers of Hanoi: to move disks from tower1 to tower3 using "buffer" tower2 to make
+    sure that, during the process, smaller disks are always on top of larger ones.
+    
+    Disks are represented as integers representing the relative size of each element, so that disk 'm' is larger than disk 'n'
+    iff m > n
     """
-
+    # Recursive algorithm
     # base case
     if len(tower1) == 2:
         tower2.push(tower1.pop())
@@ -38,10 +40,6 @@ def solve(tower1: Stack, tower2: Stack, tower3: Stack) -> Stack:
     smaller_case = Stack(tower1.arr[1:len(tower1.arr)])
     for _ in range(len(tower1)-1):
         tower1.pop()
-    solve(smaller_case, tower2, tower3)
-
-    tower2.push(tower1.pop())
-    solve(tower3, tower2, tower1)
-    tower3.push(tower2.pop())
-    solve(tower1, tower2, tower3)
-    return
+    solve(smaller_case, tower3, tower2)
+    tower3.push(tower1.pop())
+    solve(tower2, tower1, tower3)
