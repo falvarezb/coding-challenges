bc3aff2bd52b4efffe4dac2410e50c1a3ed1b0e6
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-07-11 17:18:49 +0100

    mergesort

--- python/merge_sorted_lists/merge_sorted_list.py
+++ python/merge_sorted_lists/merge_sorted_list.py
@@ -1,4 +1,4 @@
-def solution(l1: list, l2: list)->list:
+def merge_list(l1: list, l2: list) -> list:
     """
     Merge two sorted lists, the result must be sorted too
     O(N)
@@ -14,12 +14,12 @@ def solution(l1: list, l2: list)->list:
     result = []
     while(ptr1 < len(l1) and ptr2 < len(l2)):
         if l1[ptr1] <= l2[ptr2]:
-            result.append(l1[ptr1])                        
+            result.append(l1[ptr1])
             ptr1 += 1
         else:
             result.append(l2[ptr2])
             ptr2 += 1
-    
+
     if ptr1 == len(l1):
         result.extend(l2[ptr2:len(l2)])
     else:
@@ -27,3 +27,9 @@ def solution(l1: list, l2: list)->list:
     return result
 
 
+def mergesort(l: list) -> list:
+    if len(l) == 1:
+        return l
+    left = mergesort(l[:len(l)//2])
+    right = mergesort(l[len(l)//2:])
+    return merge_list(left, right)
--- python/merge_sorted_lists/merge_sorted_list_test.py
+++ python/merge_sorted_lists/merge_sorted_list_test.py
@@ -1,7 +1,11 @@
 from merge_sorted_lists.merge_sorted_list import *
 
 def test_l1endsfirst():
-    assert solution([1,3,6,7], [2,4,5,9]) == [1,2,3,4,5,6,7,9]
+    assert merge_list([1,3,6,7], [2,4,5,9]) == [1,2,3,4,5,6,7,9]
 
 def test_l2endsfirst():
-    assert solution([1,3,6,7,10], [2,4,5,9]) == [1,2,3,4,5,6,7,9,10]
+    assert merge_list([1,3,6,7,10], [2,4,5,9]) == [1,2,3,4,5,6,7,9,10]
+
+
+def test_mergesort():
+    assert mergesort([1,6,2,3,8,9,4]) == [1,2,3,4,6,8,9]
