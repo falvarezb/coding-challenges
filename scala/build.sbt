e3ae7759eb3e11e59712dd0126f3dccc89011a43
Francisco Alvarez <fjab76@gmail.com>
2021-07-25 18:35:06 +0100

    LexicographicalOrder

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,4 +1,4 @@
-name := "scala"
+name := "coding-challenges"
 version := "0.1"
 scalaVersion := "2.12.8"
 
--- scala/src/main/scala/challenges/order/LexicographicalOrder.scala
+++ scala/src/main/scala/challenges/order/LexicographicalOrder.scala
@@ -1,6 +1,5 @@
 package challenges.order
 
-import java.util
 import scala.util.control.Breaks.{break, breakable}
 
 object LexicographicalOrder {
@@ -9,26 +8,30 @@ object LexicographicalOrder {
     println(nextElement("dkhc"))
   }
 
+  /**
+    * Given a String, find, among all possible permutations of its characters, which one is the immediately next
+    * according to the lexicographical order
+    */
   def nextElement(s: String): String = {
     val arr = s.toCharArray
-    var finalJ = 0
+    var changedIdx = 0
     breakable {
-      for (i <- arr.length - 1 to(0, -1)) {
-        for (j <- i to(0, -1)) {
-          if (arr(j) < arr(i)) {
+      for (i <- arr.length - 2 to(0, -1)) {
+        for (j <- arr.length - 1 to(i+1, -1)) {
+          if (arr(i) < arr(j)) {
             val temp = arr(j)
             arr(j) = arr(i)
             arr(i) = temp
-            finalJ = j
+            changedIdx = i
             break()
           }
         }
       }
+      return "no answer"
     }
-    val left = arr.take(finalJ+1)
-    val right = arr.takeRight(s.length-finalJ-1)
-    util.Arrays.sort(right)
-    (left ++ right).toList.toString()
+    val left = arr.take(changedIdx+1)
+    val right = arr.takeRight(arr.length-changedIdx-1)
+    (left ++ right.sorted).toList.mkString
   }
 
 }
