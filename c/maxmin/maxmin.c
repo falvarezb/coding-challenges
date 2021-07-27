c6fcf7026a3a23af4c8186ef353a18db7eef7a2e
Francisco Alvarez <fjab76@gmail.com>
2021-07-27 23:14:16 +0100

    c idiomatic maxmin

--- c/maxmin/maxmin.c
+++ c/maxmin/maxmin.c
@@ -3,8 +3,8 @@
 
 int compar(const void *p1, const void *p2)
 {
-    const int* intp1 = (const int *)p1;
-    const int* intp2 = (const int *)p2;
+    const int *intp1 = (const int *)p1;
+    const int *intp2 = (const int *)p2;
 
     return (*intp1 > *intp2) - (*intp1 < *intp2);
 }
@@ -13,13 +13,17 @@ int maxMin(int k, int arr_count, int *arr)
 {
     qsort(arr, arr_count, sizeof(int), compar);
     int min_distance = 1000000000;
+    int *p1 = arr;
+    int *p2 = arr + k - 1;
+    int counter = k - 1;
 
-    for (size_t i = 0; i < arr_count - k + 1; i++)
+    while (counter++ < arr_count)
     {
-        int d = arr[i + k - 1] - arr[i];
+        int d = *p2++ - *p1++;
         if (d < min_distance)
-            min_distance = d;        
+            min_distance = d;
     }
+
     return min_distance;
 }
 
@@ -27,7 +31,8 @@ int main(int argc, char const *argv[])
 {
     int arr[] = {1, 4, 7, 2};
     printf("result=%d\n", maxMin(2, 4, arr));
-    int test_case_16[] = {100,200,300,350,400,401,402};
+    printf("result=%d\n", maxMin(1, 4, arr));
+    int test_case_16[] = {100, 200, 300, 350, 400, 401, 402};
     printf("result=%d\n", maxMin(3, 7, test_case_16));
     return 0;
 }
