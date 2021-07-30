de17a56a6f65602398165bb95e93bef99acd08dd
Francisco Alvarez <fjab76@gmail.com>
2021-07-30 22:46:19 +0100

    closes point refactoring

--- python/closest_points/closest_points_solution.py
+++ python/closest_points/closest_points_solution.py
@@ -7,12 +7,17 @@ Given n points in the plane, find the pair that is closest together.
 import math
 
 
-def distance(p1, p2):
+def distance(p1: tuple, p2: tuple) -> float:
+    """
+    each tuple represents a point (x,y) in the plane
+    """
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 
 def quadratic_solution(points):
     """
+    brute force algorithm, O(n^2)
+
     points: list of tuples, each tuple representing a point (x,y) in the plane
     """
     min_distance = math.inf
@@ -25,16 +30,16 @@ def quadratic_solution(points):
     return min_pair
 
 
-class PEntry:
+class PyElement:
     """
-    point: point (x,y) in the plane
-    x_position: position of (x,y) in the list of points sorted by coordinate x
-    y_position: position of (x,y) in the list of points sorted by coordinate y
+    Object representing elemlents of Py (list of points sorted by coordinate y):
+    - point: point (x,y) in the plane
+    - x_position: position of (x,y) in the list of points Px sorted by coordinate x    
     """
 
     def __init__(self, point, x_position):
         self.point = point
-        self.x_position = x_position        
+        self.x_position = x_position
 
     def __repr__(self) -> str:
         return f"[{self.point}, {self.x_position}]"
@@ -44,75 +49,110 @@ class PEntry:
 
 
 def sort_points(points):
+    """
+    points: list of tuples, each tuple representing a point (x,y) in the plane
+
+    returns the tuple (Px, Py) where:
+    Px = list of points ordered by coordinate x
+    Py = list of points ordered by coordinate y
+    """
     Px = sorted(points, key=lambda p: p[0])
-    Px_aux = [PEntry(p, i) for i, p in enumerate(Px)]
+    Px_aux = [PyElement(p, i) for i, p in enumerate(Px)]
     Py = sorted(Px_aux, key=lambda pentry: pentry.point[1])
     return Px, Py
 
 
 def left_half_points(Px, Py):
+    """
+    returns the values Px and Py corresponding to the left half of the points
+    """
     n = len(Px)
     left_half_upper_bound = math.ceil(n/2)
 
-    Qx, Qy = Px[:left_half_upper_bound], []    
+    newPx, newPy = Px[:left_half_upper_bound], []
     for pentry in Py:
-        if pentry.x_position < left_half_upper_bound:                        
-            Qy.append(pentry)            
-    return Qx, Qy
+        if pentry.x_position < left_half_upper_bound:
+            newPy.append(pentry)
+    return newPx, newPy
 
 
 def right_half_points(Px, Py):
+    """
+    returns the values Px and Py corresponding to the right half of the points
+    """
     n = len(Px)
     right_half_lower_bound = math.floor(n/2)
 
-    Rx, Ry = Px[right_half_lower_bound:], []    
+    newPx, newPy = Px[right_half_lower_bound:], []
     for pentry in Py:
-        if pentry.x_position >= right_half_lower_bound:            
-            Ry.append(PEntry(pentry.point, pentry.x_position-right_half_lower_bound))            
-    return Rx, Ry
+        if pentry.x_position >= right_half_lower_bound:
+            newPy.append(PyElement(pentry.point, pentry.x_position-right_half_lower_bound))
+    return newPx, newPy
 
+
+def get_candidates(left_half, Py, min_left_distance, min_right_distance):
+    """
+    candidates to be the closest points lie within 'min_distance_upper_bound' of
+    the middle point separating the left and right halves  
+    """
+
+    rightmost_left_point = left_half[-1]
+    min_distance_upper_bound = min(min_left_distance, min_right_distance)
+    candidates = []
+    for p in Py:
+        if abs(p.point[0]-rightmost_left_point[0]) < min_distance_upper_bound:
+            candidates.append(p)
+    return min_distance_upper_bound, candidates
 
 def nlogn_solution(points):
     """
-    divide and conquer algorithm
+    divide and conquer algorithm, O(n log n)
+
+    points: list of tuples, each tuple representing a point (x,y) in the plane
     """
 
     def closest_points(Px, Py):
         """
         Px: list of points sorted by coordinate x
         Py: list of points sorted by coordinate y
+
+        Recursive function, each iteration halves the input, O(log n)
         """
         if len(Px) == 2:
             return Px[0], Px[1]
 
-        Qx, Qy = left_half_points(Px, Py)
+        Lx, Ly = left_half_points(Px, Py)
         Rx, Ry = right_half_points(Px, Py)
 
-        q1, q2 = closest_points(Qx, Qy)
+        # closest points in the left half
+        l1, l2 = closest_points(Lx, Ly)
+        min_left_distance = distance(l1, l2)
+        # closest points in the right half
         r1, r2 = closest_points(Rx, Ry)
+        min_right_distance = distance(r1, r2)
 
-        delta = min(distance(q1,q2), distance(r1,r2))
-        rightmost_Qx = Qx[-1]
-        s = []
-        for p in Py:
-            if distance(p.point,rightmost_Qx) < delta:
-                s.append(p)
-        
-        min_distance = delta        
-        for i in range(len(Py)-1):
-            for j in range(i+1, min(len(Py),i+16)):
-                d = distance(Py[i].point, Py[j].point)
+        # min_distance = min(min_left_distance, min_right_distance)
+        # rightmost_left_point = Lx[-1]
+        # candidates = []
+        # for p in Py:
+        #     if abs(p.point[0]-rightmost_left_point[0]) < min_distance:
+        #         candidates.append(p)
+        min_distance_upper_bound, candidates = get_candidates(Lx, Py, min_left_distance, min_right_distance)
+        min_distance = min_distance_upper_bound
+
+        for i in range(len(candidates)-1):
+            for j in range(i+1, min(len(candidates), i+16)):
+                d = distance(candidates[i].point, candidates[j].point)
                 if d < min_distance:
                     min_distance = d
-                    min_pair = (Py[i].point, Py[j].point)
+                    min_pair = (candidates[i].point, candidates[j].point)
 
-        if min_distance < delta:
+        if min_distance < min_distance_upper_bound:
             return min_pair
-        elif distance(q1,q2) < distance(r1,r2):
-            return (q1,q2)
+        elif min_left_distance < min_right_distance:
+            return (l1, l2)
         else:
-            return (r1,r2)                
-
+            return (r1, r2)
 
     return closest_points(*sort_points(points))
 
--- python/closest_points/closest_points_test.py
+++ python/closest_points/closest_points_test.py
@@ -5,11 +5,11 @@ from random import sample
 
 
 def test_quadratic_solution():
-    points = [(0, 0), (1, 1), (2, 1), (2, 4), (9,9)]
+    points=[(0, 1), (0, 3), (2, 0), (0, 0)]
     assert quadratic_solution(points) == ((1,1),(2,1))
 
 def test_nlogn_solution():
-    points = [(0, 0), (1, 1), (2, 1), (2, 4), (9,9)]
+    points=[(0, 1), (0, 3), (2, 0), (0, 0)]
     assert nlogn_solution(points) == ((1,1),(2,1))
 
 
@@ -17,27 +17,27 @@ def test_sort_points():
     points = [(0, 0), (3, 4), (2, 5), (1, 4)]
     Px, Py = sort_points(points)
     assert Px == [(0, 0), (1, 4), (2, 5), (3, 4)]
-    assert Py == [PEntry((0, 0), 0), PEntry((1, 4), 1), PEntry((3, 4), 3), PEntry((2, 5), 2)]
+    assert Py == [PyElement((0, 0), 0), PyElement((1, 4), 1), PyElement((3, 4), 3), PyElement((2, 5), 2)]
 
 
 def test_left_half_even():
     p1, p2, p3, p4 = (0, 0), (1, 4), (2, 5), (3, 4)
     Px = [p1, p2, p3, p4]
-    Py = [PEntry(p1, 0), PEntry(p2, 1), PEntry(p3, 2), PEntry(p4, 3)]
+    Py = [PyElement(p1, 0), PyElement(p2, 1), PyElement(p3, 2), PyElement(p4, 3)]
     newPx, newPy = left_half_points(Px, Py)
     assert newPx == [p1,p2]
-    assert newPy == [PEntry(p1, 0), PEntry(p2, 1)]
+    assert newPy == [PyElement(p1, 0), PyElement(p2, 1)]
 
 
 def test_right_half_even():
     p1, p2, p3, p4 = (0, 0), (1, 4), (2, 5), (3, 4)
     Px = [p1, p2, p3, p4]
-    Py = [PEntry(p1, 0), PEntry(p2, 1), PEntry(p3, 2), PEntry(p4, 3)]
+    Py = [PyElement(p1, 0), PyElement(p2, 1), PyElement(p3, 2), PyElement(p4, 3)]
     newPx, newPy = right_half_points(Px, Py)
     assert newPx == [p3,p4]
-    assert newPy == [PEntry(p3, 0), PEntry(p4, 1)]
+    assert newPy == [PyElement(p3, 0), PyElement(p4, 1)]
 
 
-@given(st.lists(st.tuples(st.integers(0,100), st.integers(0,10)), min_size=2, max_size=50))
+@given(st.lists(st.tuples(st.integers(0,100), st.integers(0,100)), min_size=2, max_size=100, unique=True))
 def test_solution_comparison(points):
     assert distance(*quadratic_solution(points)) == distance(*nlogn_solution(points))
