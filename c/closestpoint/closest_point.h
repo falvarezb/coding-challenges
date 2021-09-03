a22ab6f36bdf9fbdc04c6e6a7f1c097ebffb2b08
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-09-03 16:59:07 +0100

    refactoring

--- c/closestpoint/closest_point.h
+++ c/closestpoint/closest_point.h
@@ -2,6 +2,18 @@
 #define FAB_CLOSEST_POINT
 
 #include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
 
 typedef struct
 {
@@ -22,18 +34,6 @@ typedef struct
     float distance;
 } points_distance;
 
-PyElement *get_candidates_from_different_halves(point reference, PyElement Py[], size_t length, size_t *candidates_length, float min_distance_upper_bound);
-void sort_points(point P[], size_t length, PyElement Py[]);
-
-//num_processes added to unify interface and simplify signature of testing methods
-points_distance nlogn_solution(point P[], size_t length, int num_processes);
-
-//num_processes added to unify interface and simplify signature of testing methods
-points_distance quadratic_solution(point P[], size_t length, int num_processes);
-
-points_distance nlogn_solution_multiproc(point P[], size_t length, int num_processes);
-points_distance nlogn_solution_multithread(point P[], size_t length, int num_processes);
-
 typedef struct
 {
     point* Px;
@@ -46,5 +46,28 @@ typedef struct
 #define errExit(msg) do { \
     perror(msg); exit(EXIT_FAILURE); \
 } while (0)
+
+#define MIN(x, y) ((x < y) ? x : y)
+
+
+// ==== AUXILIARY FUNCTIONS ====
+float distance(point p1, point p2);
+PyElement *get_candidates_from_different_halves(point reference, PyElement Py[], size_t length, size_t *candidates_length, float min_distance_upper_bound);
+void sort_points(point P[], size_t length, PyElement Py[]);
+points_distance closest_points_from_different_halves(PyElement P[], size_t length);
+void populateLy(PyElement *Py, size_t Pylength, PyElement *Ly, size_t left_half_upper_bound);
+void populateRy(PyElement *Py, size_t Pylength, PyElement *Ry, size_t right_half_lower_bound);
+
+
+// =====  SOLUTIONS ====
+//num_processes added to unify interface and simplify signature of testing methods
+points_distance quadratic_solution(point P[], size_t length, int num_processes);
+
+//num_processes added to unify interface and simplify signature of testing methods
+points_distance nlogn_solution(point P[], size_t length, int num_processes);
+
+//parallel versions of the nlogn solution
+points_distance nlogn_solution_multiproc(point P[], size_t length, int num_processes);
+points_distance nlogn_solution_multithread(point P[], size_t length, int num_processes);
 
 #endif
---
+++ c/closestpoint/util.c
@@ -0,0 +1,157 @@
+#include "closest_point.h"
+
+/* Examine a wait() status using the W* macros */
+void printWaitStatus(const char *msg, int status)
+{
+    if (msg != NULL)
+        printf("%s", msg);
+
+    if (WIFEXITED(status))
+    {
+        printf("child exited, status=%d\n", WEXITSTATUS(status));
+    }
+    else if (WIFSIGNALED(status))
+    {
+        printf("child killed by signal %d (%s)",
+               WTERMSIG(status), strsignal(WTERMSIG(status)));
+#ifdef WCOREDUMP /* Not in SUSv3, may be absent on some systems */
+        if (WCOREDUMP(status))
+            printf(" (core dumped)");
+#endif
+        printf("\n");
+    }
+    else if (WIFSTOPPED(status))
+    {
+        printf("child stopped by signal %d (%s)\n",
+               WSTOPSIG(status), strsignal(WSTOPSIG(status)));
+
+#ifdef WIFCONTINUED /* SUSv3 has this, but older Linux versions and \
+                       some other UNIX implementations don't */
+    }
+    else if (WIFCONTINUED(status))
+    {
+        printf("child continued\n");
+#endif
+    }
+    else
+    { /* Should never happen */
+        printf("what happened to this child? (status=%x)\n",
+               (unsigned int)status);
+    }
+}
+
+/**
+ * Euclidean distance
+ */
+float distance(point p1, point p2)
+{
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+int compar_point(const void *p1, const void *p2)
+{
+    const point *point1 = (const point *)p1;
+    const point *point2 = (const point *)p2;
+    return (point1->x > point2->x) - (point1->x < point2->x);
+}
+
+int compar_PyElem(const void *p1, const void *p2)
+{
+    const PyElement *point1 = (const PyElement *)p1;
+    const PyElement *point2 = (const PyElement *)p2;
+    return (point1->p.y > point2->p.y) - (point1->p.y < point2->p.y);
+}
+
+/**
+ * P -> Px, Py
+ */
+void sort_points(point P[], size_t length, PyElement Py[])
+{
+    //P is sorted in place and becomes Px
+    mergesort(P, length, sizeof(point), compar_point);
+
+    //building Py
+    for (size_t i = 0; i < length; i++)
+    {
+        PyElement py = {*(P + i), i};
+        *(Py + i) = py;
+    }
+    mergesort(Py, length, sizeof(PyElement), compar_PyElem);
+}
+
+/**
+ * Special cases:
+ * -> returns 0 elements when there are repeat points
+ * -> returns 1 element when the only candidate is the point used as reference
+ * 
+ */
+PyElement *get_candidates_from_different_halves(point reference, PyElement Py[], size_t length, size_t *candidates_length, float min_distance_upper_bound)
+{
+    PyElement *candidates = (PyElement *)malloc(length * sizeof(PyElement));
+    if (candidates == NULL)
+        return NULL;
+
+    size_t j = 0;
+    for (size_t i = 0; i < length; i++)
+        if (abs((Py + i)->p.x - reference.x) < min_distance_upper_bound)
+            *(candidates + j++) = *(Py + i);
+
+    *candidates_length = j;
+    return candidates;
+}
+
+points_distance closest_points_from_different_halves(PyElement P[], size_t length)
+{
+    points_distance closest_points;
+
+    // CAUTION: guard to avoid size_t overflow
+    // --> length = 0 when there are repeat points
+    // --> length = 1 when the only candidate is the point
+    //      used as reference to determine the candidates
+    if (length < 2)
+    {
+        closest_points.distance = INFINITY;
+        return closest_points;
+    }
+
+    float min_distance = INFINITY;
+    float d;
+
+    // CAUTION: size_t is unsigned => if length=0, length-1 is positive
+    for (size_t i = 0; i < length - 1; i++)
+    {
+        for (size_t j = i + 1; j < MIN(length, i + 16); j++)
+        {
+            d = distance(P[i].p, P[j].p);
+            if (d < min_distance)
+            {
+                min_distance = d;
+                closest_points.p1 = P[i].p;
+                closest_points.p2 = P[j].p;
+                closest_points.distance = d;
+            }
+        }
+    }
+
+    return closest_points;
+}
+
+void populateLy(PyElement *Py, size_t Pylength, PyElement *Ly, size_t left_half_upper_bound)
+{
+    size_t j = 0;
+    for (size_t i = 0; i < Pylength; i++)
+        if ((Py + i)->xposition < left_half_upper_bound)
+            *(Ly + j++) = *(Py + i);
+}
+
+void populateRy(PyElement *Py, size_t Pylength, PyElement *Ry, size_t right_half_lower_bound)
+{
+    size_t j = 0;
+    for (size_t i = 0; i < Pylength; i++)
+        if ((Py + i)->xposition >= right_half_lower_bound)
+        {
+            PyElement py = {(Py + i)->p, (Py + i)->xposition - right_half_lower_bound};
+            *(Ry + j++) = py;
+        }
+}
+
