1ab143be8a13e24df8fbc2e65c060a749a3e4162
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2024-04-28 21:01:13 +0200

    sort

--- java/src/test/java/falvarezb/RandomDataGen.java
+++ java/src/test/java/falvarezb/RandomDataGen.java
@@ -1,10 +1,10 @@
 package falvarezb;
 
 public class RandomDataGen {
-    public static int[] generateRandomArray(int size) {
+    public static int[] generateRandomArray(int size, int digits) {
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
-            arr[i] = (int) (Math.random() * 100);
+            arr[i] = (int) (Math.random() * Math.pow(10, digits));
         }
         return arr;
     }
--- java/src/test/java/falvarezb/SortTest.java
+++ java/src/test/java/falvarezb/SortTest.java
@@ -8,15 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SortTest {
 
-    //int[] expected = {1,2,3,4,6,7};
-    Sort[] sorters = {BubbleSort::sort, SelectionSort::sort, InsertionSort::sort, QuickSort::sort, MergeSort::sort};
-    String[] sorterNames = {"BubbleSort", "SelectionSort", "InsertionSort", "QuickSort", "MergeSort"};
+    Sort[] sorters = {/*BubbleSort::sort, SelectionSort::sort, */InsertionSort::sort, QuickSort::sort, MergeSort::sort};
+    String[] sorterNames = {/*"BubbleSort", "SelectionSort", */"InsertionSort", "QuickSort", "MergeSort"};
     String[] sorterLambdaNames = Arrays.stream(sorters).map(sorter -> sorter.getClass().getSimpleName()).toArray(String[]::new);
+
+    private String getSorterName(Sort sorter) {
+        return sorterNames[Arrays.binarySearch(sorterLambdaNames, sorter.getClass().getSimpleName())];
+    }
 
     private int[] runTest(int[] arr, Sort sorter) {
         int[] temp = Arrays.copyOf(arr, arr.length);
         int[] sorted = sorter.sort(temp);
         return sorted;
+    }
+
+    private void runPerfTest(int[] arr, Sort sorter) {
+        int[] temp = Arrays.copyOf(arr, arr.length);
+        long startTime = System.currentTimeMillis();
+        sorter.sort(temp);
+        long endTime = System.currentTimeMillis();
+        System.out.println(getSorterName(sorter) + ": " + (endTime - startTime) + " ms");
     }
 
     @Test
@@ -27,7 +38,7 @@ class SortTest {
         for (Sort sorter: sorters) {
             int[] sorted = runTest(arr, sorter);
             if(!Arrays.equals(expected, sorted)) {
-                System.out.println(sorterNames[Arrays.binarySearch(sorterLambdaNames, sorter.getClass().getSimpleName())]);
+                System.out.println(getSorterName(sorter));
                 System.out.println(Arrays.toString(sorted));
             }
             assertArrayEquals(expected, sorted);
@@ -38,8 +49,9 @@ class SortTest {
 
     @Test
     public void randomTest() {
+        int d = 3;
         for (int i = 0; i < 50; i++) {
-            int[] arr = RandomDataGen.generateRandomArray(10000);
+            int[] arr = RandomDataGen.generateRandomArray(1000, d);
             int[] expected = Arrays.copyOf(arr, arr.length);
             Arrays.sort(expected);
 
@@ -51,25 +63,37 @@ class SortTest {
                 }
                 assertArrayEquals(expected, sorted);
             }
-            int[] sorted = RadixSort.sort(arr, 2, 99);
+            int[] sorted = RadixSort.sort(arr, d, (int) (0.9 * Math.pow(10, d)));
             assertArrayEquals(expected, sorted);
         }
     }
 
-
     @Test
-    public void quickSortTestRepeat() {
-        int[] arr = {1,4,3,4,2,1,7,6};
-        QuickSort.sort(arr);
-        assertArrayEquals(new int[] {1,1,2,3,4,4,6,7}, arr);
+    public void perfTest() {
+        int d = 2;
+        //for (int i = 0; i < 50; i++) {
+            int[] arr = RandomDataGen.generateRandomArray(400000, d);
+            //Arrays.sort(arr);
+            for (Sort sorter : sorters) {
+                runPerfTest(arr, sorter);
+            }
+        long startTime = System.currentTimeMillis();
+            RadixSort.sort(arr, d, (int) (0.9 * Math.pow(10, d)));
+        long endTime = System.currentTimeMillis();
+        System.out.println("RadixSort" + ": " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        Arrays.sort(arr);
+        endTime = System.currentTimeMillis();
+        System.out.println("Library sort" + ": " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        Arrays.parallelSort(arr);
+        endTime = System.currentTimeMillis();
+        System.out.println("Library parallel sort" + ": " + (endTime - startTime) + " ms");
+        //}
     }
 
-    @Test
-    public void quickSortTest2Elem() {
-        int[] arr = {1,2};
-        QuickSort.sort(arr);
-        assertArrayEquals(new int[]{1,2}, arr);
-    }
 
     @Test
     public void countSortTest() {
